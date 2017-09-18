69cfc557d4c78bf5d1ba17b18b2d2588a2a742e0
Xuan Hung <luyendh@nal.vn>
2017-09-18 11:41:01 +0700

    Create and get list video structure.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-function isValidForm() {
-	var isValid = true;
-	// Handle form.
-	var videoId = document.getElementById("video-id").value;
-	if(videoId.length < 10){
-		isValid = false;
-		document.getElementById("video-id-error").innerHTML = "* Vui lòng nhập video id có độ dài lớn hơn 10 ký tự.";
-		//alert("Vui lòng nhập video id có độ dài lớn hơn 10 ký tự.");
-	} else {
-		document.getElementById("video-id-error").innerHTML = "Hợp lệ.";
-		document.getElementById("video-id-error").className = "msg-success";
-	}
-	isValid = false;
-	return isValid;
+var apiUrl = "http://youtube-video-api-1608.appspot.com/youtube/api";
+
+function handleForm() {	
+	var videoId = $("#video-id").val();
+	var videoName = $("#video-name").val();
+	var videoDescription = $("#description").val();
+	var videoKeywords = $("#keywords").val();
+
+	var video = {
+		"videoId": videoId,
+		"name": videoName,
+		"description": videoDescription,
+		"keywords": videoKeywords
+	};
+
+	$.ajax(
+	{
+	    url: apiUrl, // Địa chỉ api.
+	    data: JSON.stringify(video), // Chuyển đổi kiểu dữ liệu về json.
+	    type: 'POST', // method của phương thức gọi.
+	    success: function(data, status) { 		    	
+	    	alert("Lưu thành công video với tên: " + videoName); 		    	
+		},
+	    error: function() { 
+	    	alert('Lỗi không thể lưu dữ liệu!'); 	
+		}
+	});
+}
+
+function loadVideo(){
+	$.ajax(
+	{
+	    url: apiUrl, // Địa chỉ api.	    
+	    type: 'GET', // method của phương thức gọi.
+	    success: function(data, status) {
+	    	var htmlContent = '';
+	    	for (var i = data.length - 1; i >= 0; i--) {
+	    		console.log();
+	    		console.log(data[i].name);
+	    		htmlContent += '<div>';
+	    		htmlContent += '<div>Id: ' + data[i].videoId + '</div>';
+	    		htmlContent += '<div>Name:' + data[i].name +'</div>';
+	    		htmlContent += '</div>';														
+	    	}
+	    	$("#main").html(htmlContent);
+		},
+	    error: function() { 
+	    	alert('Lỗi không thể lấy dữ liệu từ API!'); 	
+		}
+	});
 }
